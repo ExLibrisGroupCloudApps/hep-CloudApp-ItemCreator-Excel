11e3cbafc655723293d2fbefe1f4ff87c2aec72d
Keren <sfez.keren@gmail.com>
2024-06-05 14:59:27 +0300

    add holding id and mms id to the error messag when does not exist

--- cloudapp/src/app/main/item.service.ts
+++ cloudapp/src/app/main/item.service.ts
@@ -17,29 +17,33 @@ export class ItemService {
     if(!(item.mms_id && item.holding_id)){
       return of(this.handleError({ok:false, status:"", message:"Keys mms_id and holding_id are needed", statusText:"", error:true}, item));
     }
-    let url= `/bibs/${item.mms_id}/holdings/${item.holding_id}/items`;
+    const mms_id = item.mms_id;
+    const holding_id = item.holding_id
+    let url= `/bibs/${mms_id}/holdings/${holding_id}/items`;
     const itemToSend = this.getItemToSend(item);
     console.log(itemToSend);
     return this.restService.call(url).pipe(
       switchMap(resp => {
         console.log(resp, item);
         if(resp.item == null && resp.total_record_count > 0) {
-          return of(this.handleError({ok:false, status:"", message:"Mms_id does not exist", statusText:"", error:true}, item));
+          return of(this.handleError({ok:false, status:"", message:`Mms ID ${mms_id} does not exist`, statusText:"", error:true}, item));
         }
         return this.restService.call({
         url,
         method: HttpMethod.POST,
         requestBody: itemToSend
       }).pipe(
-        catchError(e => of(this.handleError(e, item)))
+        catchError(e => {
+          if(e.message.includes("Check holdings")) {
+            e.message = `Holding ID ${holding_id} does not exist`;
+          }
+          return of(this.handleError(e, item));
+        })
       )}
     ));
   }
   
   public handleError(e: RestErrorResponse, item: any) {
-    if(e.message.includes("Check holdings")) {
-      e.message = "Holding_id does not exist";
-    }
     if (item) {
       e.message = e.message + `\n(${JSON.stringify(item)})`;
     }
